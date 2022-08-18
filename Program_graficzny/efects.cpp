d32688bbdfcfc098843d7e9915afceabfc45d735
Filip Kasprzyk <filkasp46@gmail.com>
2022-08-18 15:16:32 +0200

    efekty menu FK
    dodanie efektow i menu

--- Program_graficzny/efects.cpp
+++ Program_graficzny/efects.cpp
@@ -1,2 +1,22 @@
 #include "efects.h"
-x	
+efects::efects(bool tak) {
+	if (tak==true)
+	{
+		std::cout << "Wybierz filtr:" << std::endl
+			<< "1.Czarnobialy" << std::endl
+			<< "2.Sepia" << std::endl <<
+			"3.Rozmazanie" << std::endl;
+		int filter_name;
+		switch (filter_name)
+		{
+		case 1:
+
+		case 2:
+		case 3:
+		
+		default:
+			break;
+		}
+	}
+
+};
--- Program_graficzny/efects.h
+++ Program_graficzny/efects.h
@@ -1,7 +1,15 @@
 #pragma once
-
-
+#include <string>
+#include <iostream>
 class efects
 {
+	int moc;
+	bool tak;
+public:
+	efects(bool tak);
+	
 };
+
+
+
 
