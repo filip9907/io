3b82dd9cea919dca923f20827d99a1dcbdca818b
Filip Kasprzyk <filkasp46@gmail.com>
2022-08-20 13:57:22 +0200

    zmiana koncepcji

--- Program_graficzny/efects.cpp
+++ Program_graficzny/efects.cpp
@@ -1,34 +1,6 @@
-#include "efects.h"
-efects::efects(bool tak) {
-	if (tak==true)
-	{
-		std::cout << "Wybierz filtr:" << std::endl
-			<< "1.Czarnobialy" << std::endl
-			<< "2.Sepia" << std::endl <<
-			"3.Rozmazanie" << std::endl;
-		int filter_name=0;
-		std::cin >> filter_name;
-		switch (filter_name)
-		{
-		case 1:
-			std::cout << "wybrales czarnobialy";
-			break;
-
-		case 2:
-			std::cout << "wybrales sepie";
-			break;
-		case 3:
-			std::cout << "wybrales rozmazanie";
-			break;
-		default:
-			break;
-		}
-	}
-
-};
-
-int efects::moc_filtra(int power) {
-
-	std::cout << "moc filtra:" << power << std::endl;
-
+class efects
+{
+public:
+    virtual ~efects() = default;
+    virtual std::efects doAlgorithm(std::string_view data) const = 0;
 };
--- Program_graficzny/main.cpp
+++ Program_graficzny/main.cpp
@@ -1,12 +1,97 @@
 #include <iostream>
-#include<vector>
-#include "efects.h"
-#include "menu.h"
 #include <string>
+class Efekt
+{
+public:
+    virtual ~Efekt() = default;
+    virtual std::string doAlgorithm(std::string_view data) const = 0;
+};
 
-int main() {
-	int a;
-	efects::moc_filtra(a);
+/**
+ * The BlackAndWhite defines the interface of interest to clients.
+ */
 
-	return 0;
+class BlackAndWhite
+{
+ 
+private:
+    std::unique_ptr<Efekt> strategy_;
+    /**
+     * Usually, the BlackAndWhite accepts a Efekt through the constructor, but also
+     * provides a setter to change it at runtime.
+     */
+public:
+    explicit BlackAndWhite(std::unique_ptr<Efekt>&& Efekt = {}) : strategy_(std::move(Efekt))
+    {
+    }
+    /**
+     * Usually, the BlackAndWhite allows replacing a Efekt object at runtime.
+     */
+    void set_strategy(std::unique_ptr<Efekt>&& Efekt)
+    {
+        strategy_ = std::move(Efekt);
+    }
+    /**
+     * The BlackAndWhite delegates some work to the Efekt object instead of
+     * implementing +multiple versions of the algorithm on its own.
+     */
+    void doSomeBusinessLogic() const
+    {
+        if (strategy_) {
+            std::cout << "BlackAndWhite: Sorting data using the Efekt (not sure how it'll do it)\n";
+            std::string result = strategy_->doAlgorithm("aecbd");
+            std::cout << result << "\n";
+        }
+        else {
+            std::cout << "BlackAndWhite: Efekt isn't set\n";
+        }
+    }
+};
+
+/**
+ * Concrete Strategies implement the algorithm while following the base Efekt
+ * interface. The interface makes them interchangeable in the BlackAndWhite.
+ */
+class ConcreteStrategyA : public Efekt
+{
+public:
+    std::string doAlgorithm(std::string_view data) const override
+    {
+        std::string result(data);
+        std::sort(std::begin(result), std::end(result));
+
+        return result;
+    }
+};
+class ConcreteStrategyB : public Efekt
+{
+    std::string doAlgorithm(std::string_view data) const override
+    {
+        std::string result(data);
+        std::sort(std::begin(result), std::end(result), std::greater<>());
+
+        return result;
+    }
+};
+/**
+ * The client code picks a concrete Efekt and passes it to the BlackAndWhite. The
+ * client should be aware of the differences between strategies in order to make
+ * the right choice.
+ */
+
+void clientCode()
+{
+    BlackAndWhite BlackAndWhite(std::make_unique<ConcreteStrategyA>());
+    std::cout << "Client: Efekt is set to normal sorting.\n";
+    BlackAndWhite.doSomeBusinessLogic();
+    std::cout << "\n";
+    std::cout << "Client: Efekt is set to reverse sorting.\n";
+    BlackAndWhite.set_strategy(std::make_unique<ConcreteStrategyB>());
+    BlackAndWhite.doSomeBusinessLogic();
+}
+
+int main()
+{
+    clientCode();
+    return 0;
 }
