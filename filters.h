c9dfc6b96abe12f3db6ac8a511168b2ee4c6dd80
mgromek99 <56263838+mgromek99@users.noreply.github.com>
2022-08-23 17:57:46 +0200

    NOWA KLASA DZIAUA
    OOOOOOOO

---
+++ filters.h
@@ -0,0 +1,5 @@
+#pragma once
+
+class filters {};
+class Sphere : public Shape { ... };
+class Plane : public Shape { ... };
--- testujemywczytywanie.cpp
+++ testujemywczytywanie.cpp
@@ -1,19 +1,23 @@
 #pragma once
-
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <filesystem>
-
+#include <vector>
+#include "filter.h"
+#include "filter_add.h"
+#include "filter_median.h"
+#include "filter_min.h"
+#include "filter_max.h"
 // <
 // 1-5 (tryb)
 // >
 // 
 // <
-// if filter -> wielkosc 1,3,5,7...
+// if filter (0) -> wielkosc 1,3,5,7...
 // >
 // <
-// if add -> r,g,b
+// if add (1) -> r,g,b
 // > i tyle, dalej filter
 //
 //<
@@ -21,21 +25,38 @@
 //>
 //<
 //<
-//if filter specajlny -> koniec (sam sie wypelnia jedynkami)
+//if filter specajlny (2,3,4) -> koniec (sam sie wypelnia jedynkami)
 //>
 namespace fs = std::filesystem;
+
 int main()
 {
-    const fs::path sandbox{ "sandbox" };
-    fs::create_directories(sandbox / "dir1" / "dir2");
-    std::ofstream{ sandbox / "file1.txt" };
-    std::ofstream{ sandbox / "file2.txt" };
+    const fs::path rozszerzenia { "rozszerzenia" };
+    fs::create_directories(rozszerzenia);
+    //std::ofstream{ sandbox / "file1.txt" };
 
     std::cout << "directory_iterator:\n";
     // directory_iterator can be iterated using a range-for loop
-    for (auto const& dir_entry : fs::directory_iterator{ sandbox })
+
+    std::vector<filter> filtry;
+
+    for (auto const& dir_entry : fs::directory_iterator{ rozszerzenia })
     {
-        std::cout << dir_entry.path() << '\n';
+        std::vector<int> wektor;
+        std::cout <<std::endl<< dir_entry.path() << '\n';
+        std::ifstream plik(dir_entry.path());
+        std::string word;
+        std::string line;
+        getline(plik, line);
+        while (plik >> word)
+        {
+           wektor.vector::push_back(std::stoi(word));
+        }
+        for (auto i = wektor.begin(); i != wektor.end(); ++i)
+            std::cout << *i << " ";
+        filter nowy(stoi(line));
+        nowy.showMatrix();
     }
+
     return 0;
 }
